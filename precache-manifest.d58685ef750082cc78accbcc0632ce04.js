67c1b5f8e524728309e27df29047a2a480139f85
Alma Negrete Shen <alma@devartisan.cl>
2020-01-13 20:17:08 -0300

    buils

---
+++ precache-manifest.d58685ef750082cc78accbcc0632ce04.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "c18d6b298093d8116507",
+    "url": "/static/build/static/js/runtime~main.c18d6b29.js"
+  },
+  {
+    "revision": "11c2218a317d2fc574f9",
+    "url": "/static/build/static/js/main.11c2218a.chunk.js"
+  },
+  {
+    "revision": "e2228a25bda2db79d9b5",
+    "url": "/static/build/static/js/2.e2228a25.chunk.js"
+  },
+  {
+    "revision": "b0b66c6855c6fc6d604271651a9d7a02",
+    "url": "/static/build/index.html"
+  }
+];
